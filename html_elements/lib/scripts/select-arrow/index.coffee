b0039bd3bc95adef89f0c90be40aaeb5c3434d8a
Tomohiro Kasuga <tk@internets.co.jp>
2016-02-04 22:19:28 +0900

    Fix select-arrow

--- html_elements/lib/scripts/select-arrow/index.coffee
+++ html_elements/lib/scripts/select-arrow/index.coffee
@@ -12,17 +12,7 @@ do ->
 
     items = document.querySelectorAll('.select__arrow')
     unless items.length then return
-
-    clicked = (ev)->
-      scope = ev.currentTarget.parentNode
-      select = scope.querySelector('select')
-      event = document.createEvent('MouseEvents')
-      event.initMouseEvent('mousedown', false, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null)
-      select.dispatchEvent(event)
-      return
-
-    for item in items by -1
-      item.addEventListener('click', clicked, false)
+    item.style.display = 'none' for item in items by -1
 
     return
   document.addEventListener('DOMContentLoaded', DOMContentLoaded, false)
--- html_elements/lib/scripts/select-arrow/index.js
+++ html_elements/lib/scripts/select-arrow/index.js
@@ -11,22 +11,14 @@
     return;
   }
   DOMContentLoaded = function() {
-    var clicked, i, item, items;
+    var i, item, items;
     items = document.querySelectorAll('.select__arrow');
     if (!items.length) {
       return;
     }
-    clicked = function(ev) {
-      var event, scope, select;
-      scope = ev.currentTarget.parentNode;
-      select = scope.querySelector('select');
-      event = document.createEvent('MouseEvents');
-      event.initMouseEvent('mousedown', false, true, window, 0, 0, 0, 0, 0, false, false, false, false, 0, null);
-      select.dispatchEvent(event);
-    };
     for (i = items.length - 1; i >= 0; i += -1) {
       item = items[i];
-      item.addEventListener('click', clicked, false);
+      item.style.display = 'none';
     }
   };
   document.addEventListener('DOMContentLoaded', DOMContentLoaded, false);
